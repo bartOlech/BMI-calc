d617eb80459e063fc59ef985c0519987709b222f
Olech Bartlomiej <olech.bartlomiej@gmail.com>
2018-08-23 20:14:12 +0200

    push first app commit

---
+++ src/js/correctWeight.js
@@ -0,0 +1,13 @@
+const yourBMI=document.querySelector('#bmiResult');
+const mobileViewport = window.matchMedia("screen and (max-width: 600px)");
+
+export const CorrectWeight=(value)=>{
+   document.querySelector('.resultStyle').classList.remove('none')
+    yourBMI.innerHTML=value;
+    document.querySelector('#correctWeight').classList.remove('none')
+    if(mobileViewport.matches) {
+        document.querySelector('.resultSection').setAttribute('style', 'animation-name:showResultPhone; animation-duration:0.9s;');
+    } else {
+        document.querySelector('.resultSection').setAttribute('style', 'animation-name:showResultLargeScreen; animation-duration:0.9s;');
+        document.querySelector('body').setAttribute('style', 'animation-name:removeOverflow; animation-duration:0.9s;')
+    }  }
